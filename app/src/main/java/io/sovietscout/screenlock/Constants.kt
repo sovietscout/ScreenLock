089a9f61184dd72b6d1b42ebc851fb1df8c363a7
Ritobhash <ritobhash@gmail.com>
2022-01-30 23:09:36 +0530

    Added new constant

--- app/src/main/java/io/sovietscout/screenlock/Constants.kt
+++ app/src/main/java/io/sovietscout/screenlock/Constants.kt
@@ -1,7 +1,7 @@
 package io.sovietscout.screenlock
 
 object Constants {
-    const val TAG = "Screen Lock"
+    const val RUN_ON_RESUME = "run_on_resume"
 
     const val SCREEN_STATE_UNLOCKED = 0
     const val SCREEN_STATE_LOCKED = 1
