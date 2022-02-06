dfb2bceabbe2fad7efcebaccc3035a0dd5c8eb8f
Ritobhash <ritobhash@gmail.com>
2022-02-06 20:51:38 +0530

    `Run on resume` variable added to handle whether to start overlay onResume of MainActivity

--- app/src/main/java/io/sovietscout/screenlock/ui/StartServiceActivity.kt
+++ app/src/main/java/io/sovietscout/screenlock/ui/StartServiceActivity.kt
@@ -3,11 +3,14 @@ package io.sovietscout.screenlock.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.sovietscout.screenlock.AppUtils
+import io.sovietscout.screenlock.Constants
 import io.sovietscout.screenlock.service.ForegroundService
 
 class StartServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        RUN_ON_RESUME = intent.getBooleanExtra(Constants.RUN_ON_RESUME, true)
 
         if (!ForegroundService.IS_SERVICE_RUNNING)
             AppUtils.startForegroundService(this)
@@ -16,4 +19,6 @@ class StartServiceActivity : AppCompatActivity() {
 
         finish()
     }
+
+    companion object { var RUN_ON_RESUME = true }
 }
